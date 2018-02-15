b7cb562c6fa44ce50df2f70bf0f26b873f4b2d71
duanyinchuan <duanyinchuan@163.com>
2018-02-15 17:03:20 +0800

    测试类

--- src/Main.java
+++ src/Main.java
@@ -4,15 +4,5 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        System.out.println("Hello World!");
-        System.out.println("Hello World!");
-        System.out.println("Hello World!");
-        System.out.println("123");
-        System.out.println("123");
-        System.out.println("123");
-        System.out.println("123");
-        System.out.println("123");
-        System.out.println("123");
-
     }
 }
